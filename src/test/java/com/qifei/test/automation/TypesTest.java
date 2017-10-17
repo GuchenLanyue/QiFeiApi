ebef879f9b2b98c9a187daadf7cb4eae58dda465
sam <sam@DESKTOP-F5F253E>
2017-10-17 19:16:19 +0800

    审批类型测试类

---
+++ src/test/java/com/qifei/test/automation/TypesTest.java
@@ -0,0 +1,15 @@
+package com.qifei.test.automation;
+
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import com.qifei.utils.test.BaseTest;
+
+public class TypesTest extends BaseTest{
+	
+	@Test(dataProvider="SingleCase")
+	public void modify_Types_Test(Map<String, Object> params){
+		
+	}
+}
