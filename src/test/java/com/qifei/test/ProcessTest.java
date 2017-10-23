f0517097c9518a55db05e915a1963c74a4319c44
sam <sam@DESKTOP-F5F253E>
2017-10-23 19:12:46 +0800

    增加验证结果判断是否需要重新运行当前测试用例

--- src/test/java/com/qifei/test/ProcessTest.java
+++ src/test/java/com/qifei/test/ProcessTest.java
@@ -8,6 +8,8 @@ import com.qifei.apis.Organization;
 import com.qifei.utils.TxtData;
 import com.qifei.utils.test.BaseTest;
 
+import io.restassured.path.json.JsonPath;
+
 public class ProcessTest extends BaseTest {
 	
 	@Test(dataProvider = "SingleCase", description= "删除子部门，为整体流程测试做准备")
@@ -23,10 +25,28 @@ public class ProcessTest extends BaseTest {
 		String caseName = baseData.get("Case").toString();
 		setRequest(api,filePath,caseName);
 		
+		long time = 5000;
+		while (checkResponse(getExpectedMap())&&time<15000) {
+			try {
+				Thread.sleep(time);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			setRequest(api,filePath,caseName);
+			time += 5000;
+		}
+		
+		if(api=="Auth"){
+			JsonPath body = JsonPath.with(getBodyStr());
+			String authorization = "Bearer " + body.getString("access_token");
+			String tokenFile = System.getProperty("user.dir")+"\\sources\\config\\access_token.txt";
+			TxtData txt = new TxtData();
+			txt.writerText(tokenFile, authorization);
+		}
+		
 		TxtData txt = new TxtData();
 		String filename = getSrcDir()+"\\temp\\"+api+".txt";
 		txt.writerText(filename, getBodyStr());
-		
-		checkResponse(getExpectedMap());
 	}
 }
